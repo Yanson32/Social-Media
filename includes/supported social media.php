c017a7c3620c5c6660f6d1cb6b54e59bdaaa8ff9
Wayne Larson Jr <wlarson30@gmail.com>
2018-06-19 00:46:34 -0300

    Removed unnecessary code

--- includes/supported social media.php
+++ includes/supported social media.php
@@ -1,7 +1,3 @@
 <?php
 $social_media = array('Linkedin', 'Twitter', 'Facebook', 'Pinterest');
 add_option('supported_social_media',$social_media);
-
-
-$social_media_handles = array('Linkedin' <= '', 'Twitter' <= '', 'Facebook' <= '', 'Pinterest' <= '');
-add_option('social_media_handles',$social_media_handles);
